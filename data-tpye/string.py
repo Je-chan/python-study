3f03e72e431cb04abf3a9c2993c98c0e4ba0e847
박예찬 <jkyyc3@gmail.com>
2022-03-19 21:51:14 +0900

    UPDATE : 포맷팅 예시 추가

--- data-tpye/string.py
+++ data-tpye/string.py
@@ -5,10 +5,12 @@
 ## 1. 문자열과 숫자를 곱하면 문자열을 반복한다.
 print('hello' * 3)
 
+
 ## 2. [] 를 사용해서 인덱스 값에 맞춰서 찾을 수 있다. 음수도 가능
 a = 'Life is short, you need python'
 print(a[1]) 
 print(a[-6])
+
 
 ## 3. 슬라이싱 하는 방법은 : 를 사용한다
 print(a[5:]) # is short, you need python
@@ -16,8 +18,10 @@ print(a[:5]) # Life
 print(a[1:5]) # ife
 print(a[2:-2]) # fe is short, you need pyth
 
+
 ## 4. 기본적으로 문자열은 데이터를 변경할 수 없어 슬라이싱을 많이 사용한다.
 ### a[0] = 'R' <= 이렇게 한 글자만 바꿀 수 없다는 것
+
 
 ## 5. 문자열과 관련된 함수
 ### 5-1. len : String 의 길이 값을 반환한다.
@@ -55,6 +59,13 @@ print('지금은 {time}시 {minute}분입니다.'.format(time=8, minute=50))
 ### 6-2. 두 번째 방법 : %
 #### %d : 정수 / %s : 문자열 / %f : 실수 / %o : 8진수 / %x : 16진수
 print('지금은 %d시 %d분입니다' % (7, 29))
+
+#### 
+num1 = 12
+num2 = 3.567
+s1 = '성공'
+## 0.2 를 사용하면 소수점 둘 째자리 까지만 사용하겠다는 것을 의미
+print("%d + %0.2f = %0.2f %s" % (num1, num2, num1 + num2, s1))  # 12 + 3.57 = 15.57 성공
 
 
 ## 7. 형 변환
